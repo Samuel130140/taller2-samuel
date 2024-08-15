384e12300b3f38ef9266bda2877bf385c3037514
Samuel130140 <s.pedrozag@uniandes.edu.co>
2024-08-14 20:40:06 -0500

    Cambio de color y tamaño histograma e inclución de función de probabilidad

--- Archivo Ejemplo.py
+++ Archivo Ejemplo.py
@@ -10,9 +10,23 @@ import pandas as pd
 df = pd.DataFrame(vals)
 
 import matplotlib.pyplot as plt
-count, bins, ignored = plt.hist(x=vals, bins=30, edgecolor = "black", color="red")
+count, bins, ignored = plt.hist(x=vals, bins=15, edgecolor = "black", color="Blue")
 plt.title('Histograma de tiempos de servicio Exponencial')
 plt.xlabel('Tiempos de servicio')
 plt.ylabel('Frecuencia')
 #plt.savefig("Hist Exponencial Original.png")
 plt.show()
+
+from scipy.stats import expon
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+x= np.arange(-0,5,0.001)
+plt.plot(x, expon.pdf(x, scale = 1/3), color = "red")
+plt.title(r'Función de densidad Exponencial $\lambda = 3$')
+plt.xlabel('Valores')
+plt.ylabel('Densidad')
+#plt.savefig("Lambda3.png")
+plt.show()
+
